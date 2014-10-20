c277430ad10f45c87e557643d1e66bf0d1f48421
Grzegorz Solecki <gregory.solecki@gmail.com>
2014-10-21 00:18:53 +0100

    Renames location of thymeleaf.

--- src/test/java/sample/ui/ServerControllerWebTests.java
+++ src/test/java/sample/ui/ServerControllerWebTests.java
@@ -15,7 +15,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
@@ -24,7 +24,7 @@ import org.springframework.web.context.WebApplicationContext;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @WebAppConfiguration
-@SpringApplicationConfiguration(classes = WebApp.class)
+@ContextConfiguration(classes = WebApp.class)
 public class ServerControllerWebTests {
 
 	@Autowired
